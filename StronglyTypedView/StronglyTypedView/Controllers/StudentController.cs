cc3474372b285370cc52a0809dba1eab0ff8bd95
sandeepforever007 <snalla@uncc.edu>
2017-09-26 23:33:02 -0500

    initial commit

---
+++ StronglyTypedView/StronglyTypedView/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StronglyTypedView.Models;
+
+namespace StronglyTypedView.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+			Student student = new Student
+			{
+				ID = 1,
+				FirstName = "Sandeep",
+				LastName = "Nalla",
+				City = "Coppell"
+			};
+			return View(student);
+        }
+    }
+}
